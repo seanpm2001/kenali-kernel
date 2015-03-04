458d8288cd15303179a076631355c4d4264e8633
Chengyu Song <csong84@gatech.edu>
2015-03-03 21:04:41 -0500

    temporarily disable it for turning on DATA_PROTECTION

--- kernel/cred.c
+++ kernel/cred.c
@@ -562,7 +562,7 @@ void __init cred_init(void)
 {
 	/* allocate a slab in which we can store credentials */
 	cred_jar = kmem_cache_create("cred_jar", sizeof(struct cred),
-				     0, SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_SENSITIVE, NULL);
+				     0, SLAB_HWCACHE_ALIGN|SLAB_PANIC/*|SLAB_SENSITIVE*/, NULL);
 }
 
 /**
